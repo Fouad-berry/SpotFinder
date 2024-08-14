518bd04ae2524eaad94ec4a1f330a88d3259f3ed
Fouad-berry <fidriss96@gmail.com>
2024-08-14 13:53:43 +0200

    add search bar style to the Header

--- .expo/types/router.d.ts
+++ .expo/types/router.d.ts
@@ -6,7 +6,7 @@ export * from 'expo-router';
 declare module 'expo-router' {
   export namespace ExpoRouter {
     export interface __routes<T extends string = string> extends Record<string, unknown> {
-      StaticRoutes: `/` | `/Navigations/TabNavigation` | `/Screens/Fav` | `/Screens/Home` | `/Screens/Profile` | `/Screens/Search` | `/_sitemap`;
+      StaticRoutes: `/` | `/Components/Home/Header` | `/Navigations/TabNavigation` | `/Screens/Fav` | `/Screens/Home` | `/Screens/Profile` | `/Screens/Search` | `/_sitemap`;
       DynamicRoutes: never;
       DynamicRouteTemplate: never;
     }
--- app/Components/Home/Header.js
+++ app/Components/Home/Header.js
@@ -1,12 +1,21 @@
 import { View, Text, Image } from 'react-native'
 import React from 'react'
-import { StyleSheet } from 'react-native-web'
+import { StyleSheet, TextInput } from 'react-native-web'
+import { SearchBar } from 'react-native-screens'
 
 export default function Header() {
   return (
     <View>
         <Image source={require('../../../assets/images/logo.png')}
-        style={styles.logo}
+        style={styles.logo}/>
+
+        <View>
+            <TextInput placeholder='Search'
+                style={styles.searchBar}
+            />
+        </View>
+        <Image source={require('../../../assets/images/placeholder.jpg')}
+            style={styles.userImage}
         />
     </View>
   )
@@ -16,5 +25,17 @@ const styles = StyleSheet.create({
     logo:{
         width:50,
         height:50
+    },
+    searchBar:{
+        borderWidth:1,
+        borderColor:'#000',
+        padding:4,
+        borderRadius:50,
+        paddingLeft:10
+    },
+    userImage:{
+        width:50,
+        height:50,
+        borderRadius:100
     }
 })

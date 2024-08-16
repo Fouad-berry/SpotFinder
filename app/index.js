cb053314e870e759c3bff531942b20aca508e44b
Fouad-berry <fidriss96@gmail.com>
2024-08-16 22:21:34 +0200

    setup google map for homepage

--- app/index.js
+++ app/index.js
@@ -2,11 +2,32 @@ import { NavigationContainer } from "@react-navigation/native";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import TabNavigation from "./Navigations/TabNavigation";
+import { useEffect, useState } from "react";
+import * as Location from "expo-location";
+import { UserLocationContext } from "./Context/UserLocationContext";
 
 export default function App() {
+    const [location, setLocation] = useState(null);
+    const [errorMsg, setErrorMsg] = useState(null);
+    useEffect(() => {
+        (async () => {
+            let { status } = await Location.requestForegroundPermissionsAsync();
+            if (status !== 'granted') {
+            setErrorMsg('Permission to access location was denied');
+            return;
+            }
+    
+            let location = await Location.getCurrentPositionAsync({});
+            setLocation(location);
+        })();
+    }, []);
+    
     return (
         <View style={styles.container}>
+            <UserLocationContext.Provider 
+            value={{location,setLocation}}>
                 <TabNavigation/>
+            </UserLocationContext.Provider>
         </View>
     );
 }
